e58efdf7af65a27e99c0daf597de10ebda976196
Martyn Gigg <martyn.gigg@gmail.com>
2022-10-20 09:15:38 +0100

    Deploy nightly documentation (manually)

--- nightly/plotting/3DPlotsHelp-2.py
+++
@@ -1,8 +0,0 @@
-from mantid.simpleapi import *
-import matplotlib.pyplot as plt
-
-data = Load('MAR11060.nxs')
-
-fig, ax = plt.subplots(subplot_kw={'projection':'mantid3d'})
-ax.plot_wireframe(data, color='#1f77b4')
-plt.show()
