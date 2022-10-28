eda431964b7832fddc99e32f2c6fe77a8c487dc9
Martyn Gigg <martyn.gigg@gmail.com>
2022-10-28 08:17:43 +0100

    Deploy nightly documentation (manually)

--- nightly/_static/documentation_options.js
+++ nightly/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.5.20221026.1619',
+    VERSION: '6.5.20221027.1652',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
