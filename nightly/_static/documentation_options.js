7ab45e7086d2427c4afca0ab70d5fbb3cfb8c5f6
Martyn Gigg <martyn.gigg@gmail.com>
2022-11-21 06:36:31 +0000

    Update nightly documentation

--- nightly/_static/documentation_options.js
+++ nightly/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.5.20221115.1731',
+    VERSION: '6.5.20221118.1339',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
