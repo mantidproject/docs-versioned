d2a8df2674ec76a90ab0a91a9a88f2cc9b68b60a
Martyn Gigg <martyn.gigg@gmail.com>
2023-02-06 13:31:00 +0000

    Remove nightly documentation
    This is now hosted in a separate
    Git repository

--- nightly/plotting/index-10.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import mantid.plots.axesfunctions as axesfuncs
-from mantid.simpleapi import Load
-w=Load('CNCS_7860')
-fig, ax = plt.subplots(subplot_kw={'projection':'mantid'})
-ax.plot(w,LogName='ChopperStatus5')
-axt=ax.twiny()
-axesfuncs.plot(axt,w,LogName='ChopperStatus5', FullTime=True)
-#fig.show()
--- nightly/techniques/DirecttoolsPythonModule-1.py
+++
@@ -1,8 +0,0 @@
-import directtools as dt
-from mantid.simpleapi import *
-
-DirectILLCollectData(Run='ILL/IN4/084447.nxs', OutputWorkspace='data')
-DirectILLReduction(InputWorkspace='data', OutputWorkspace='SofQW')
-
-fig, ax = dt.plotSofQW('SofQW')
-#fig.show()
